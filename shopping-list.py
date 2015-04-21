5c309eb75a94996fdd48dd851d9d9cd030796710
JEverhart383 <jeffeverhart383@gmail.com>
2015-04-21 17:06:53 -0400

    initial commit

---
+++ shopping-list.py
@@ -0,0 +1,23 @@
+shopping_list = list()
+
+
+def show_help():
+	print("What should we pick up at the store?")
+	print("Enter 'Done' to stop adding items.")
+
+
+
+while True:
+	new_item = input(">")
+
+	if new_item == "done":
+		break
+
+	shopping_list.append(new_item)
+	print("Added! List has {} items.".format(len(shopping_list)))
+	continue 
+
+print("Here's your list: ")
+
+for item in shopping_list: 
+	print(item)		
