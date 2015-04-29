ca1380d965388c4ff0fb82b6b94a18b1c36a9197
JEverhart383 <jeffeverhart383@gmail.com>
2015-04-29 11:08:51 -0400

    update function

--- shopping-list.py
+++ shopping-list.py
@@ -1,23 +1,30 @@
-shopping_list = list()
+# shopping_list = list()
 
+
+# def show_help():
+# 	print("What should we pick up at the store?")
+# 	print("Enter 'Done' to stop adding items.")
+
+
+
+# while True:
+# 	new_item = input(">")
+
+# 	if new_item == "done":
+# 		break
+
+# 	shopping_list.append(new_item)
+# 	print("Added! List has {} items.".format(len(shopping_list)))
+# 	continue 
+
+# print("Here's your list: ")
+
+# for item in shopping_list: 
+# 	print(item)		
+
+shopping_list = []
 
 def show_help():
-	print("What should we pick up at the store?")
-	print("Enter 'Done' to stop adding items.")
+	print("Seperate each item with a comma.")
+	print("Type Done to quit, etc")
 
-
-
-while True:
-	new_item = input(">")
-
-	if new_item == "done":
-		break
-
-	shopping_list.append(new_item)
-	print("Added! List has {} items.".format(len(shopping_list)))
-	continue 
-
-print("Here's your list: ")
-
-for item in shopping_list: 
-	print(item)		
